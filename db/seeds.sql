f7d98dee0643da370783e8f07c8922b666d2e399
Miguel Jimenez <mjimenez08@gmail.com>
2020-08-29 17:09:22 -0400

    fixed left over cat app stuff

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,8 @@
 -- INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
 -- INSERT INTO cats (name) VALUES ('Owens');
 
+-- INSERT INTO burger (name) VALUES ('BACONATOR');
+-- INSERT INTO cats (name) VALUES ('Bobbi');
+-- INSERT INTO cats (name, devoured) VALUES ('Sylvester', true);
+
+
