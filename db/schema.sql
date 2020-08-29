7599d3fa77075a703e68249e91e375097a18dd1f
Miguel Jimenez <mjimenez08@gmail.com>
2020-08-29 16:18:05 -0400

    basic function working

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,25 @@
+-- ### Schema
+
+-- CREATE DATABASE cat_db;
+-- USE cat_db;
+
+-- CREATE TABLE cats
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	name varchar(255) NOT NULL,
+-- 	sleepy BOOLEAN DEFAULT false,
+-- 	PRIMARY KEY (id)
+-- );
+
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE cats
+CREATE TABLE burger
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	devour BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
